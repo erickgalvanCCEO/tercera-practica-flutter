bb0655241c918317d01905ce8d8bd7a704a39bf6
CCEO Software <cceosoftware@MacBook-Air-de-CCEO.local>
2022-04-20 14:56:38 -0500

    Work in progress :construction_worker: Addesd files to create the connection to the Movies API

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movie_provider.dart';
 import 'package:peliculas/routes/routes.dart';
 import 'package:peliculas/theme/theme.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MovieProvider(),
+          lazy: false,
+        ),
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
