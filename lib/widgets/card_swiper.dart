234d5f6ea03145f4d8277dd848cce0c99f59b86c
CCEO Software <cceosoftware@MacBook-Air-de-CCEO.local>
2022-04-21 13:18:53 -0500

    new features :sparkles: Images of movies playinng are shown on main section

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,33 +1,41 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:peliculas/models/movie.dart';
 import 'package:peliculas/theme/theme.dart';
 
 class CardSwiper extends StatelessWidget {
-  const CardSwiper({Key? key}) : super(key: key);
+  const CardSwiper({
+    Key? key,
+    required this.movies,
+  }) : super(key: key);
+
+  final List<Movies> movies;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    List lista = [1, 2, 3, 45];
+    List lista = [];
+    movies.forEach((element) {
+      lista.add(element.fullPosterImg);
+    });
     return Container(
       width: double.infinity,
       height: size.height * 0.5,
       child: CarouselSlider(
           options: CarouselOptions(disableCenter: true),
-          items: lista
-              .map((i) => GestureDetector(
-                    onTap: () => Navigator.pushNamed(context, 'details',
-                        arguments: 'movie-instance'),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: const FadeInImage(
-                          image: NetworkImage(
-                              'https://via.placeholder.com/300x400'),
-                          placeholder: NetworkImage(
-                              'https://via.placeholder.com/300x400'),
-                        )),
-                  ))
-              .toList()),
+          items: lista.map((i) {
+            return GestureDetector(
+              onTap: () => Navigator.pushNamed(context, 'details',
+                  arguments: 'movie-instance'),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: FadeInImage(
+                    image: NetworkImage(i),
+                    placeholder:
+                        NetworkImage('https://via.placeholder.com/300x400'),
+                  )),
+            );
+          }).toList()),
     );
   }
 }
